364227c418b4b1ff837dbb1828e8f55ebaf68a7d
Rakib099 <rakibnabi97@gmail.com>
2022-11-12 12:17:42 +0600

    button added to cards

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -7,7 +7,10 @@ const Exercise = ({exercise}) => {
         <div className='exercise-card'>
             <img src={exercise.img} alt="exercise-img" />
             <h4 className='exercise-title'>{exercise.title}</h4>
-            <p>Duration: </p>
+            <p>Duration: <span>{exercise.duration} secs</span></p>
+            <button className='btn-add'>
+                <p>Add to list</p>
+            </button>
         </div>
     );
 };
