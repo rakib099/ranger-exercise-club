2964756fe911b29c4d807bd4880ee78f046c7039
Rakib099 <rakibnabi97@gmail.com>
2022-11-12 15:32:33 +0600

    made add a break functional

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SideBar.css';
 
 const SideBar = ({time}) => {
-    
+    const [breakTime, setBreakTime] = useState(0);
     const totalTime = time.reduce((previous, current) => {
         return previous + current;
     }, 0);
-    console.log(totalTime)
-    const breaks = [10, 20, 30, 40, 50];
-    console.log();
+    // console.log(totalTime)
+
+    const breaks = [
+        {key: 1, time: 10},
+        {key: 2, time: 20},
+        {key: 3, time: 30},
+        {key: 4, time: 40}
+    ];
+    
+    const handleAddBreak = (breakTime) => {
+        setBreakTime(breakTime);
+    }
+
     return (
         <div className='side-info'>
             <div className="personal-info">
                 <h2>Name</h2>
             </div>
-            <h4>Add a break</h4>
+            <h3>Add a break</h3>
             <div className="break-container">
                 {
-                    breaks.map(b => <p>{b}</p>)
+                    breaks.map(b => <p
+                        onClick={() => handleAddBreak(b.time)}
+                        key={b.key}
+                        >{b.time}</p>)
                 }
             </div>
             <div className="exercise-details">
-                <h4>Exercise Details</h4>
+                <h3>Exercise Details</h3>
                 <p className='exercise-time'>Total time: <span>{totalTime} seconds</span></p>
-                <p>Break time: <span></span></p>
+                <p>Break time: <span>{breakTime} seconds</span></p>
             </div>
             <button className='btn-complete'>
                 <p>Activity Completed</p>
